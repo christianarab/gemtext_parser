15cd26774f943dde1fa6c74c0a9375329041da42
christian <rewindforward@gmail.com>
2021-11-23 01:28:55 -0500

    Adds check_format, changes parse

--- app.rb
+++ app.rb
@@ -1,31 +1,38 @@
 module Gemtext
   module Parser
-    # Gemini links
-    # ["=> "]
-
-    # Gemini headings
-    # ["# ", "## ", "### "]
-
-    # Gemini list
-    # ["* "]
-
-    # Gemini quote
-    # ["> "]
-
-    # Gemini pre-formatted text
-    # ["```", "```"]
-
     # Parses input. Raises error if input is not a string.
     def parse input
       if input.length < 0 or !input.is_a? String
         raise StandardError.new "Error: Not a string!"
       else
         parsed_output = []
-        input.each_line { |ln| parsed_output << ln.chomp }
+        input.each_line do |ln|
+          pair = Hash.new
+          pair["input"] = ln.chomp
+          pair["type"] = check_format ln
+          parsed_output << pair
+        end
         parsed_output
       end
     end
-    
+
+    def check_format input
+      case true  
+        when is_h1?(input)
+          "h1"
+        when is_h2?(input)
+          "h2"
+        when is_h3?(input)
+          "h3"
+        when is_bq?(input)
+          "bq"
+        when is_li?(input)
+          "li"
+        else
+          "uf"
+      end
+    end
+
     # True if input is gemini text level-1-heading. Otherwise, false.
     def is_h1? input
       if input.start_with? "# "
