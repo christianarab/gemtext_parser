24a596709ffa3ab232a4beab1d04be77e75186a3
christian <rewindforward@gmail.com>
2021-11-28 08:37:35 -0500

    Changes parse methods

--- lib/parse_h1.rb
+++ lib/parse_h1.rb
@@ -6,6 +6,6 @@ module ParseH1
   private 
   # Parses level-1-heading. If input starts with "# ", return string without type prefix. Else, input.
   def parse_h1 input
-    is_h1?(input) ? input[2..-2] : input.chomp
+    is_h1?(input) ? input[PrefixH1.length..-2] : input.chomp
   end
 end
--- lib/parse_h3.rb
+++ lib/parse_h3.rb
@@ -6,6 +6,6 @@ module ParseH3
   private
   # Parses level-3-heading. If input starts with "### ", return string without type prefix. Else, input.
   def parse_h3 input
-    is_h3?(input) ? input[4..-2] : input.chomp
+    is_h3?(input) ? input[PrefixH3.length..-2] : input.chomp
   end
 end
