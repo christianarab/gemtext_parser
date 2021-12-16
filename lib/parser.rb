8409ec8a68af1fc3ca08c6af1c6120f03722e90e
christian <rewindforward@gmail.com>
2021-12-16 01:12:17 -0500

    Adds gemtext_lines method

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,13 +1,17 @@
 module Parser
+
   private
+
+  def gemtext_lines string
+    string.split(/(\n)/).each_slice(2).map{ |e| e.join.chomp }
+  end
+
   # Parses input. Raises error if input is not a string.
   def parse input
     if !input.is_a? String
       raise StandardError.new "Error: Not a string!"
     else
-      parsed_output = []
-      input.each_line { |ln| parsed_output << ln.chomp }
-      parsed_output
+      gemtext_lines input
     end
   end
 end
