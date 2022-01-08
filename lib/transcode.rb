1d2f6f7773556d3e539844013efdb2fbc343f4f1
christian <rewindforward@gmail.com>
2022-01-08 09:24:59 -0500

    Fixes transcode.rb
    Fix

--- lib/transcode.rb
+++ lib/transcode.rb
@@ -1,21 +1,24 @@
 require_relative 'render_html'
 require_relative 'render_terminal'
 require_relative 'parse_lines'
+require_relative 'gemtext_lines'
 
 module Transcode
   include RenderTerminal
   include RenderHTML
   include ParseLines
+  include GemtextLines
 
   private
 
-  def transcode(input = [], output_type)
-    line = parse_lines input 
+  def transcode(input, output_type)
+    input = gemtext_lines input 
+    lines = parse_lines input
     case output_type
     when 'terminal'
-      render_terminal line
+      render_terminal lines
     when 'html'
-      render_html line
+      render_html lines
     else
       StandardError.new "Incorrect output type. Must be 'terminal', or 'html'."
     end
--- test/transcode_test.rb
+++ test/transcode_test.rb
@@ -8,12 +8,12 @@ class TranscodeTest < Test::Unit::TestCase
     @transcode_examples = Array.new
 
     ex_1 = Hash.new
-    ex_1["result"] = transcode ["# testing", "## testing"], "terminal"
+    ex_1["result"] = transcode("# testing\n## testing", "terminal")
     ex_1["expected"] = ["\e[41m\e[1m\e[30mtesting\e[0m", "\e[31m\e[1mtesting\e[0m"]
     @transcode_examples << ex_1
 
     ex_2 = Hash.new
-    ex_2["result"] = transcode ["# testing", "## testing"], "html"
+    ex_2["result"] = transcode("# testing\n## testing", "html")
     ex_2["expected"] = "<!DOCTYPE html>\n" +
     "<html>\n" +
     "<head></head>\n" +
@@ -25,14 +25,14 @@ class TranscodeTest < Test::Unit::TestCase
     @transcode_examples << ex_2
 
     ex_3 = Hash.new
-    ex_3["result"] = transcode ["# Gemtext", "### Link", "=> www.example.com/link This is the link."], "terminal"
+    ex_3["result"] = transcode("# Gemtext\n### Link\n=> www.example.com/link This is the link.", "terminal")
     ex_3["expected"] = ["\e[41m\e[1m\e[30mGemtext\e[0m",
       "\e[1mLink\e[1m\e[0m",
       "\e[48;2;255;199;9m\e[30m⇒ www.example.com/link This is the link.\e[0m"]
     @transcode_examples << ex_3
 
     ex_4 = Hash.new
-    ex_4["result"] = transcode ["# Gemtext", "### Link", "=> www.example.com/link This is the link."], "html"
+    ex_4["result"] = transcode("# Gemtext\n### Link\n=> www.example.com/link This is the link.", "html")
     ex_4["expected"] = "<!DOCTYPE html>\n" +
     "<html>\n" +
     "<head></head>\n" +
@@ -45,25 +45,27 @@ class TranscodeTest < Test::Unit::TestCase
     @transcode_examples << ex_4
 
     ex_5 = Hash.new
-    ex_5["result"] = transcode ["> Quote", "Cherry. # Apples, # Oranges", "* list item"], "terminal"
-    ex_5["expected"] = ["“Quote”", "Cherry. # Apples, # Oranges", "• list item"]
+    ex_5["result"] = transcode("> Quote\nCherry.\n# Apples\n# Oranges\n* list item", "terminal")
+    ex_5["expected"] = ["“Quote”", "Cherry.", "\e[41m\e[1m\e[30mApples\e[0m", "\e[41m\e[1m\e[30mOranges\e[0m", "• list item"]
     @transcode_examples << ex_5
 
     ex_6 = Hash.new
-    ex_6["result"] = transcode ["> Quote", "Cherry. # Apples, # Oranges", "* list item"], "html"
+    ex_6["result"] = transcode("> Quote\nCherry.\n# Apples\n# Oranges\n* list item", "html")
     ex_6["expected"] = "<!DOCTYPE html>\n" +
     "<html>\n" +
     "<head></head>\n" +
     "<body>\n" +
     "<blockquote>Quote</blockquote>\n" +
-    "<p>Cherry. # Apples, # Oranges</p>\n" +
+    "<p>Cherry.</p>\n" +
+    "<h1>Apples</h1>\n" +
+    "<h1>Oranges</h1>\n" +
     "<li>list item</li>\n" +
     "</body>\n" +
     "</html>"
     @transcode_examples << ex_6
 
     ex_7 = Hash.new
-    ex_7["result"] = transcode ["# H1", "## H2", "### H3", "* list item", "=> www.example.com link", "> block quote", "p text"], "terminal"
+    ex_7["result"] = transcode("# H1\n## H2\n### H3\n* list item\n=> www.example.com link\n> block quote\np text", "terminal")
     ex_7["expected"] = ["\e[41m\e[1m\e[30mH1\e[0m",
       "\e[31m\e[1mH2\e[0m",
       "\e[1mH3\e[1m\e[0m",
@@ -74,7 +76,7 @@ class TranscodeTest < Test::Unit::TestCase
     @transcode_examples << ex_7
 
     ex_8 = Hash.new
-    ex_8["result"] = transcode ["# H1", "## H2", "### H3", "* list item", "=> www.example.com link", "> block quote", "p text"], "html"
+    ex_8["result"] = transcode("# H1\n## H2\n### H3\n* list item\n=> www.example.com link\n> block quote\np text", "html")
     ex_8["expected"] = "<!DOCTYPE html>\n" +
     "<html>\n" +
     "<head></head>\n" +
@@ -91,12 +93,12 @@ class TranscodeTest < Test::Unit::TestCase
     @transcode_examples << ex_8
 
     ex_9 = Hash.new
-    ex_9["result"] = transcode ["=> www.example.com example", "123"], "terminal"
+    ex_9["result"] = transcode("=> www.example.com example\n123", "terminal")
     ex_9["expected"] = ["\e[48;2;255;199;9m\e[30m⇒ www.example.com example\e[0m", "123"]
     @transcode_examples << ex_9
 
     ex_10 = Hash.new
-    ex_10["result"] = transcode ["=> www.example.com example", "123"], "html"
+    ex_10["result"] = transcode("=> www.example.com example\n123", "html")
     ex_10["expected"] = "<!DOCTYPE html>\n" +
     "<html>\n" +
     "<head></head>\n" +
